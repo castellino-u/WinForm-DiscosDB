37695791aae8f04da16171d0f0bd91514d0447b8
oscar <castellino.u@gmail.com>
2025-09-15 14:06:11 -0300

    Add project files.

---
+++ Proyecto_DiscosV1/Disco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_DiscosV1
+{
+    internal class Disco
+    {
+        public string Artista { get; set; }
+
+        public string Album  { get; set; }
+
+        public string UrlImagenTapa { get; set; }
+
+
+
+    }
+}
